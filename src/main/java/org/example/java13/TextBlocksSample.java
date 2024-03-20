15af2a1d01b149c2a7a0a13f15068af03c5541ab
ramazansakin <ramazansakin63@gmail.com>
2024-03-20 07:19:17 +0300

    feat: Java13 - Switch Expression sample usage

--- src/main/java/org/example/java13/TextBlocksSample.java
+++ src/main/java/org/example/java13/TextBlocksSample.java
@@ -1,2 +1,15 @@
-package org.example.java13;public class TextBlocksSample {
+package org.example.java13;
+
+public class TextBlocksSample {
+    public static void main(String[] args) {
+        String sqlQuery = """
+                    SELECT *
+                    FROM employees
+                    WHERE department = 'IT'
+                    ORDER BY last_name
+                """;
+
+        System.out.println(sqlQuery);
+    }
+
 }
