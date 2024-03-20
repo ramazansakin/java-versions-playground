d08308892454d09e6c858c28085de0268fe522b0
ramazansakin <ramazansakin63@gmail.com>
2024-03-20 07:18:59 +0300

    feat: Java13 - Text Blocks sampel usage

--- src/main/java/org/example/java13/SwitchExpressionWithYield.java
+++ src/main/java/org/example/java13/SwitchExpressionWithYield.java
@@ -1,2 +1,23 @@
-package org.example.java13;public class SwitchExpressionWithYield {
+package org.example.java13;
+
+public class SwitchExpressionWithYield {
+
+    public static void main(String[] args) {
+
+        int day = 3;
+        String dayType = switch (day) {
+            case 1, 2, 3, 4, 5 -> "Weekday";
+            case 6, 7 -> "Weekend";
+            default -> {
+                if (day > 0 && day < 8) {
+                    yield "Invalid Day";
+                } else {
+                    yield "Unknown Day";
+                }
+            }
+        };
+
+        System.out.println("Day Type: " + dayType);
+    }
+
 }
