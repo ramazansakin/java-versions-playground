ecc08ffa473972af374c26dea647327b35d7a69c
ramazansakin <ramazansakin63@gmail.com>
2024-05-15 08:13:19 +0300

    fix: StreamAPIUp - an issue resolved

--- src/main/java/org/example/java8/streams/StreamAPIUp.java
+++ src/main/java/org/example/java8/streams/StreamAPIUp.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
-// Exercises - Up
+// Exercises
 // 1- Filtering with Multiple Conditions: Filter the list to find all people who are older than 25 years and live in either "New York" or "Los Angeles".
 // 2- Mapping with Transformation: Create a list of strings containing the names of all people in uppercase.
 // 3- Sorting with Comparator: Sort the list of people first by city in ascending order, and then by age in descending order within each city.
@@ -33,6 +33,7 @@ public class StreamAPIUp {
         people.add(new Person("David", 40, "San Francisco"));
         people.add(new Person("Ken", 37, "Los Angeles"));
         people.add(new Person("Tom", 79, "New York"));
+        people.add(new Person("ObuObuhakamitabuha:)", 39, "Uganda"));
         people.add(new Person("Jonathan", 19, "Los Angeles"));
         people.add(new Person("Jane", 29, "Los Angeles"));
 
@@ -72,7 +73,8 @@ public class StreamAPIUp {
 
 
         // 4- Grouping and Counting: Group the list of people by their city, and then count the number of people in each city.
-        people.stream().collect(Collectors.groupingBy(Person::getCity))
+        people.stream()
+                .collect(Collectors.groupingBy(Person::getCity))
                 .forEach((city, peopleList) -> System.out.printf("%s -> %d\n", city, peopleList.size()));
 
 
@@ -117,7 +119,8 @@ public class StreamAPIUp {
         System.out.println("-------------------------------------------------------------------");
 
 
-        // 8 : Partitioning: Partition the list of people into two groups: one containing people older than 30 years and the other containing people younger than or equal to 30 years.
+        // 8 : Partitioning: Partition the list of people into two groups: one containing people older than 30 years
+        //     and the other containing people younger than or equal to 30 years.
         Predicate<Person> condition = person -> person.getAge() > 30;
         Map<Boolean, List<Person>> partitioning30 = people.stream().collect(
                 Collectors.partitioningBy(
@@ -136,19 +139,24 @@ public class StreamAPIUp {
 
         System.out.println("-------------------------------------------------------------------");
 
-        // 9 : Reducing Operation: Find the person with the longest name in the list.
+        // 9 : Reducing Operation: Find the person with the longest name in the list
+
+
+        // 9 : Reducing Operation: Find the person with the longest name in the list
+
+
         Person oldestPerson = people.stream()
                 .collect(
-                        Collectors.maxBy(Comparator.comparing(Person::getAge)
+                        Collectors.maxBy(Comparator.comparing(person -> person.getName().length())
                         )
                 ).orElseThrow(() -> new RuntimeException("There is no person in the list!"));
 
         // 9-2 : alternative
         Person oldestPerson2 = people.stream()
-                .max(Comparator.comparing(Person::getAge))
+                .max(Comparator.comparing(person -> person.getName().length()))
                 .orElseThrow(() -> new RuntimeException("There is no person in the list!"));
 
-        System.out.println("Oldest Person : " + oldestPerson);
+        System.out.println("The Person with the longest name : " + oldestPerson);
 
         System.out.println("-------------------------------------------------------------------");
 
@@ -190,6 +198,5 @@ public class StreamAPIUp {
         }
 
     }
-
 
 }
