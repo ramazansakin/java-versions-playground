d045ac90e5cf51dac2905b18e7337185bcc1bb65
rsakin <ramazansakin63@gmail.com>
2025-03-18 05:37:12 +0300

    java9: modularity sample up

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,19 @@
-// Module descriptor for com.example.main
-module com.example.main {
-    exports org.example.java9;
-    requires java.net.http;
+// 1. Java Platform Module System (JPMS)
+// module-info.java file example
+module com.example.myapp {
+    requires java.logging;
+    requires java.sql;
+    exports org.example.java8.streams;
+    provides org.example.TaskService with org.example.impl.TaskServiceImpl;
 }
 
+// Modularity, as it states that, it is useful for multi-module projects' dependency encapsulation & management
 
-// Modularity, as it states taht, it is useful for multi-module projects' dependency encapsulation & management
-// Classpath Hell: Managing dependencies using the classpath could lead to conflicts between different versions of libraries, resulting in runtime errors that are hard to diagnose.
-// Visibility Issues: All classes on the classpath were visible to all other classes, leading to potential encapsulation violations and accidental usage of internal APIs.
-// Security Concerns: Lack of strong encapsulation could lead to security vulnerabilities, as classes could access and modify internal state of other classes.
+// Classpath Hell: Managing dependencies using the classpath could lead to conflicts between different versions
+// of libraries, resulting in runtime errors that are hard to diagnose.
+
+// Visibility Issues: All classes on the classpath were visible to all other classes, leading to potential encapsulation
+// violations and accidental usage of internal APIs.
+
+// Security Concerns: Lack of strong encapsulation could lead to security vulnerabilities, as classes could access
+// and modify internal state of other classes.
--- src/main/java/org/example/java9/Modularity.java
+++
@@ -1,8 +0,0 @@
-package org.example.java9;
-
-public class Modularity {
-
-    // Module needs to be defined on root folder called "java" to identify all the modules
-    // Details on described on module-info.java
-
-}
